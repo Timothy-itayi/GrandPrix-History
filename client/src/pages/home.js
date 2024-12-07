65d8b356db79db064ae24cbf8ab29b98ae547db3
Timothy-itayi <119027453+Timothy-itayi@users.noreply.github.com>
2024-12-07 20:56:11 +1100

    Update home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="overflow-hidden py-24 pb-20 animate-fade-up">
-    <div className="py-10>
+    <div className="py-10">
       <header className="text-center ">
         <div >
         <h1 className="text-7xl font-bold pb-10 text-gray-800 text-white">Welcome to GrandPrix History</h1>
